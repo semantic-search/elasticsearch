f08e1da42460300350884b4ada83ea2bc670ad34
Akshay Ashok <aks28id@gmail.com>
2020-09-18 22:00:28 +0530

    added node suggest example

---
+++ nodeExample/index.js
@@ -0,0 +1,58 @@
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://52.188.166.61:9200" });
+
+async function run() {
+  const { body: bulkResponse } = await client.bulk({
+    refresh: true,
+    body: [
+      { index: { _index: "game-of-thrones" } },
+      {
+        character: "Ned Stark",
+        quote: "Winter is coming.",
+      },
+
+      { index: { _index: "game-of-thrones" } },
+      {
+        character: "Daenerys Targaryen",
+        quote: "I am the blood of the dragon.",
+      },
+
+      { index: { _index: "game-of-thrones" } },
+      {
+        character: "Tyrion Lannister",
+        quote: "A mind needs books like a sword needs a whetstone.",
+      },
+    ],
+  });
+
+  if (bulkResponse.errors) {
+    console.log(bulkResponse);
+    process.exit(1);
+  }
+
+  const { body } = await client.search({
+    index: "game-of-thrones",
+    body: {
+      //   query: {
+      //     match: { quote: 'witner' }
+      //   },
+      suggest: {
+        gotsuggest: {
+          text: "witner",
+          term: { field: "quote" },
+        },
+      },
+    },
+  });
+
+  const {
+    suggest: {
+      gotsuggest: [data],
+    },
+  } = body;
+
+  console.log(data)
+  console.log(body);
+}
+
+run().catch(console.log);
